ce12af76eb7d30461116266fdcaf8a1db6b7b71d
katherina17 <katherinepetrova76@gmail.com>
2022-02-21 21:49:42 +0300

    init: created a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+  let reverseNum = "";
+  while(n > 0){
+      reverseNum += n % 10;
+      n = Math.floor(n / 10); 
+  }
+  return Number(reverseNum);
 }
